8417c950086597b89f900fdf31c2d622c540edf1
unknown <sanjeevexpo1@gmail.com>
2019-12-26 22:47:15 +0530

    for pushing into web

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 var myEvents = [];
 const { createWorker } = FFmpeg;
-const worker = createWorker({
-corePath: './node_modules/@ffmpeg/core/ffmpeg-core.js',
-progress: p => console.log(p),
-});
+const worker = createWorker();
 const message = document.getElementById('message');
 message.innerHTML = 'Loading ffmpeg-core.js';
 worker.load()
 message.innerHTML = 'Load complete';
 const transcode = async ({ target: { files } }) => {
 
-console.log(typeof files[0]);
+console.log(files[0]);
 const { name } = files[0];
+console.log(name)
 start_time = document.getElementById("start").value
 end_time = document.getElementById("end").value
 console.log(start_time)
@@ -29,6 +27,9 @@ video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
 
 function logEvent(e)
 {
+	
+	var {target : {files}} = e
+	console.log(files)
     e.preventDefault(); // prevent default behaviour if needed
     myEvents.push(e); // Store event info
 }
